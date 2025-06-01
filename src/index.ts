e2451621d6b9d8a4c314af037c8167262105756f
AlejandroYanes <alejandro.yanes94@gmail.com>
2025-06-01 12:40:18 +0100

    feat: implemented the first ffmpeg endpoint and command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import { serve } from "bun";
+import { serve, $ } from "bun";
 
 const server = serve({
   routes: {
-    "/hello": new Response("Hello from bunpeg!"),
+    "/ffmpeg/version": async (req) => {
+      const output = await $`ffmpeg -version`.text();
+      const parts = output.split("\n");
+      return new Response(parts[0]);
+    },
   },
   fetch(req) {
     return new Response("Hello from bunpeg!");
