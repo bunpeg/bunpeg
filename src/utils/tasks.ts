67c427bb32e7cb279cef0664c8441ab7354af7d5
AlejandroYanes <alejandro.yanes94@gmail.com>
2025-06-07 18:36:00 +0100

    chore: removed commented sqlite code

--- src/utils/tasks.ts
+++ src/utils/tasks.ts
@@ -1,4 +1,3 @@
-import { connectDb } from './db.ts';
 import type { Operations } from '../schemas.ts';
 import { nanoid } from 'nanoid';
 import { sql } from 'bun';
@@ -14,23 +13,11 @@ export interface Task {
 }
 
 export async function getTask(taskId: string) {
-  // using db = connectDb();
-  // using query = db.query<Task, string>('SELECT * FROM tasks WHERE id = ?');
-  // return query.get(taskId);
-
   const query = await sql`SELECT * FROM tasks WHERE id = ${taskId}`;
   return query[0] as Task | undefined;
 }
 
 export async function getNextPendingTask(params: { excludeFileIds: string[] }) {
-  // using db = connectDb();
-  // using query = db.query<Task, string[]>(`
-  //   SELECT * FROM tasks
-  //   WHERE status = ?
-  //   ${params.excludeFileIds.length > 0 ? `AND file_id NOT IN (${params.excludeFileIds.map(() => '?').join(', ')}` : ' '}
-  //   LIMIT 1
-  // `);
-
   const fileIdsFilter = params.excludeFileIds.length > 0 ? sql`AND file_id NOT IN ${sql(params.excludeFileIds)}` : sql``;
   const query = await sql`
     SELECT *
@@ -41,18 +28,10 @@ export async function getNextPendingTask(params: { excludeFileIds: string[] }) {
 }
 
 export async function getTasksForFile(fileId: string) {
-  // using db = connectDb();
-  // using query = db.query<Task, string>('SELECT * FROM tasks WHERE file_id = ?');
-  // return query.all(fileId);
-
   return (await sql`SELECT * FROM tasks WHERE file_id = ${fileId}`) as Task[];
 }
 
 export async function createTask(fileId: string, operation: Task['operation'], args: Operations) {
-  // using db = connectDb();
-  // using query = db.query('INSERT INTO tasks (id, file_id, status, operation, args) VALUES (?, ?, ?, ?, ?)');
-  // query.run(nanoid(8), fileId, 'queued', operation, JSON.stringify(args));
-
   await sql`INSERT INTO tasks ${sql({
     id: nanoid(8),
     file_id: fileId,
@@ -75,27 +54,14 @@ export async function bulkCreateTasks(tasks: { fileId: string, operation: Task['
 }
 
 export async function updateTask(taskId: string, task: Partial<Exclude<Task, 'id'>>) {
-  // const params = Object.keys(task).map((key) => task[key as keyof Task]!);
-  // const setParams = Object.keys(task).map((key) => `${key} = ?`).join(', ');
-  //
-  // using db = connectDb();
-  // using query = db.query(`UPDATE tasks SET ${setParams} WHERE id = ?`);
-  // query.run(...params, taskId)
-
   await sql`UPDATE tasks SET ${sql(task)} WHERE id = ${taskId}`;
 }
 
 export async function markPendingTasksForFileAsUnreachable(fileId: string) {
-  // using db = connectDb();
-  // using query = db.query<Task, string[]>('UPDATE tasks SET status = ? WHERE file_id = ? and status = ?');
-  // return query.all('unreachable', fileId, 'queued');
   await sql`UPDATE tasks SET status = 'unreachable' WHERE id = ${fileId} and status = 'queued'`;
 }
 
 export async function deleteAllTasksForFile(fileId: string) {
-  // using db = connectDb();
-  // using  query = db.query('DELETE FROM tasks WHERE file_id = ?');
-  // query.run(fileId);
   await sql`DELETE FROM tasks WHERE file_id = ${fileId}`;
 }
 
@@ -104,8 +70,8 @@ export async function restoreAllProcessingTasksToQueued() {
 }
 
 export function logTask(taskId: string, message: string) {
-  console.log('----------START---------');
+  console.log('-------------------');
   console.log('Task: ', taskId);
   console.log(message);
-  console.log('---------END----------');
+  console.log('-------------------');
 }
